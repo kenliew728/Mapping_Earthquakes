60991824953a02017fe7d602758f41c5e934306e
kenliew728 <fuiken28@hotmail.com>
2020-12-06 21:28:05 -0600

    commit with updated js and html files

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -12,8 +12,18 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     zoom: 4
 //   });
 
+// Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+let marker = L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+
+}).addTo(map);
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     // id: 'mapbox.streets',
@@ -23,12 +33,18 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// Different map style id
-// mapbox.streets
-// mapbox.light
-// mapbox.dark
-// mapbox.satellite
-// mapbox.streets-satellite
+// Different map style id available using valid access token:
+// mapbox://styles/mapbox.streets-v11
+// mapbox://styles/mapbox.light-v10
+// mapbox://styles/mapbox.dark-v10
+// mapbox://styles/mapbox.satellite-v9
+// mapbox://styles/mapbox.streets-satellite-v11
+// mapbox://styles/mapbox/navigation-preview-day-v4
+// mapbox://styles/mapbox/navigation-preview-night-v4
+// mapbox://styles/mapbox/navigation-guidance-day-v4
+// mapbox://styles/mapbox/navigation-guidance-night-v4
+
+// Different map style id available but not mentioned in module
 // mapbox.wheatpaste
 // mapbox.streets-basic
 // mapbox.comic
