715fc442e591c3607af856e897ef6f9af765518e
kenliew728 <fuiken28@hotmail.com>
2020-12-07 10:25:48 -0600

    update

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([36.1733, -120.1794], 5);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // Alternative Method --> Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
